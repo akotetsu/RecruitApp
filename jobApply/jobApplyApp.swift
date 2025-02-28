5ec64f438b7b81834bc2a25455ef8507bf9e79e4
akotetsu <yuanliqu@gmail.com>
2025-02-28 19:47:24 +0900

    Initial Commit

---
+++ jobApply/jobApplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  jobApplyApp.swift
+//  jobApply
+//
+//  Created by 原里駆 on 2025/02/28.
+//
+
+import SwiftUI
+
+@main
+struct jobApplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
